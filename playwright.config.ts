186b35f500925539172ba47ccbf1f40aa571f8fd
Lê Quang Bách <bachlqde180363@fpt.edu.vn>
2025-10-22 14:05:47 +0700

    add CI in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,23 +1,35 @@
 import { defineConfig, devices } from "@playwright/test";
 import path from "path";
 
+// Biến này sẽ là `true` khi chạy trên GitHub Actions và `false` khi chạy trên máy bạn
+const isCI = !!process.env.CI;
+
 const REPORTS_DIR = path.join(__dirname, "reports");
 
 export default defineConfig({
   testDir: "./tests",
   timeout: 30 * 1000,
-  retries: 1,
+  
+  // Chỉ chạy lại 1 lần nếu là môi trường CI, không chạy lại ở local
+  retries: isCI ? 1 : 0,
 
   fullyParallel: true,
-  workers: 5,
+  
+  // Dùng 5 workers trong CI, ở local Playwright sẽ tự quyết định số worker tối ưu
+  workers: isCI ? 5 : undefined, 
 
   outputDir: path.join(REPORTS_DIR, "test-results"),
 
   use: {
     baseURL: "https://www.tncstore.vn/",
-    headless: false,
+    
+    // QUAN TRỌNG: Tự động chạy headless trong môi trường CI
+    headless: isCI,
+    
     screenshot: "only-on-failure",
     video: "retain-on-failure",
+    // Thêm trace để dễ debug hơn khi test fail trong CI
+    trace: 'on-first-retry',
   },
 
   projects: [
